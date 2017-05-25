e52068df758b5b92d954c496b67259c96e5fe418
Zet God <usr1@Usr1s-MacBook-Pro.local>
2017-05-25 17:00:13 +0200

    zabawa z Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+
+module.exports = function(grunt) { 
+
+	// Project configuration. 
+	grunt.initConfig({ 
+		sass: { 
+			options: { 
+				sourceMap: true 
+			}, 
+			dist: { 
+				files: { 
+					'style.css': 'style.sass' 
+				} 
+			} 
+		},
+		imagemin: { 
+			dynamic: { 
+				files: [{ 
+					expand: true, 
+					//cwd: 'images/', 
+					src: ['**/*.{png,jpg,gif}'], 
+					//dest: '/images/build/' 
+				}] 
+			} 
+		},
+		watch: { 
+			scripts: { 
+				files: ['sass/*.sass'], 
+				tasks: ['sass'], 
+				options: { 
+					spawn: false, 
+				}, 
+			} 
+		},
+		browserSync: {
+    		bsFiles: {
+        		src : 'assets/css/*.css'
+    		},
+    		options: {
+        		server: {
+            		baseDir: "./"
+        		}
+    		}
+		}
+	}); 
+
+	// Load the plugins tasks 
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task(s). 
+	grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']); 
+};
